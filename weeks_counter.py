26c76bf8e7790e6fd40cb47fcbf0c4bdef2b4192
Crystal <miss.cjmc@gmail.com>
2017-07-16 15:07:50 -0700

    Weekly Counter Section

---
+++ weeks_counter.py
@@ -0,0 +1,11 @@
+weeks = 1
+
+
+print("Week {} has arrived.".format(weeks))
+counter = 0
+week =weeks[counter]
+weeks = weeks + 1
+
+# Initially I tried unsucessfuly to use %s as a string formatter, as I had
+# been taught on Codecademy. After some research I realised that %s is used
+# in Python 2.7 not Python3. Google is bae
