fb8b5b296e824c147630ca794eaf7285c6396dbe
Crystal <miss.cjmc@gmail.com>
2017-07-16 16:32:08 -0700

    Added error message from attempt #5

--- weekly_topic5.py
+++ weekly_topic5.py
@@ -7,3 +7,9 @@ topics.remove(random_topic)
 
 while topics > max_index:
 	print("Weekly Topic: " + random_topic)
+
+# error message
+
+# File "weekly_topic5.py", line 8, in <module>
+#     while topics > max_index:
+# TypeError: '>' not supported between instances of 'list' and 'int'
